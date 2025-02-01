96df4fdf690f1122f25b858c344b600c02b1d7a0
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2025-02-01 17:55:02 +0100

    typing LoaderFunction

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,12 +1,13 @@
 import { snapiCustomFetch } from "@/utils/custom-fetch"
 import { NewsResponse } from "@/utils/types"
+import { LoaderFunction } from "react-router"
 
 const newParams = {
   news_site_exclude:"SpacePolicyOnline.com",
   limit : 20,
   ordering : "-published_at"
 }
-export const newsPageLoader = async():Promise<NewsResponse| null> => {
+export const newsPageLoader:LoaderFunction = async():Promise<NewsResponse| null> => {
 try {
   const formattedParams = {
     ...newParams

f4407a026b8c9a65b01de28b739a8ff7349fa3d6
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2025-02-01 19:13:23 +0100

    styling and adding information's api

--- src/components/CardsGrid.tsx
+++ src/components/CardsGrid.tsx
@@ -4,7 +4,7 @@ import NewPageCard from './NewPageCard'
 
 const CardsGrid = ({objects , mode} : {objects: Array<News> , mode:string}): ReactNode => {
   return (
-    <div className='grid grid-cols-1 gap-y-4 auto-rows-[600px] lg:auto-row-[300px]'>
+    <div className='grid grid-cols-1 gap-y-4 auto-rows-[600px] lg:auto-rows-[300px]'>
         {objects.map((object, index) => 
 
             <NewPageCard news={object} key={index}/>
--- src/components/NewPageCard.tsx
+++ src/components/NewPageCard.tsx
@@ -1,16 +1,42 @@
 import { News } from "@/utils/types"
+import { CircleArrowRight } from "lucide-react"
 import { Link } from "react-router-dom"
 
-const NewPageCard = ({news , key} : {news : News , key:number}) => {
+const NewPageCard = ({news} : {news : News , key:number}) => {
     const {url , image_url , title , published_at , news_site , summary} = news
   return (
-    <div className="">
-        <div className="">
-            <Link to={url} target="_blank" >
-            <img key={key} src={image_url}></img>
+    <div className="test grid grid-cols-1 lg:grid-cols-4 lg:gap-x-3">
+        <div className="p-2 overflow-hidden lg:col-span-1 h-[300px] md:h-[400px] lg:h-full">
+            <Link to={url} target="_blank"  >
+
+            <img  src={image_url} alt={`${title}-img`} className=" w-full h-full object-cover "/>
             </Link>
         </div>
-        <div className=""></div>
+        <div className="p-2 overflow-hidden lg:col-span-3 h-[300px] md:h-[200px] lg:h-full">
+            <p className="text-2xl font-bold">
+                {title}
+            </p>
+
+            <div className="flex flex-col items-start">
+            <p className="">
+                {published_at.split('T')[0]}
+            </p>
+            <div className=" flex flex-row gap-x-3">
+            <span>{news_site}</span> 
+            <span className="">|</span>
+            <span className="">Read from source </span>
+            <Link to={url} target="_blank">
+            <CircleArrowRight color="purple" className="hover:scale-150 transition-all "/>
+            </Link>
+            </div>
+
+
+            <p className="mt-4">{summary}</p>
+
+            </div>
+           
+
+        </div>
     </div>
   )
 }

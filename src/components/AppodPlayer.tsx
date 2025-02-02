37b9e0f9c6e160582db861f8daa7995c60941234
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2025-02-02 18:06:53 +0100

    finish style nasa page

--- src/components/AppodPlayer.tsx
+++ src/components/AppodPlayer.tsx
@@ -28,27 +28,27 @@ const AppodPlayer = ({appod , day , setDay , isLoading} :AppodPlayerType) => {
 
   return (
     <>
-    <div className="" >
-        <button onClick={previousHandler}>
-            <CircleArrowLeft/>
+    <div className="w-full mx-auto flex justify-between items-center" >
+        <button onClick={previousHandler} className="mx-4  ">
+            <CircleArrowLeft size={36} className='hover:scale-110 transition-all ' />
         </button>
         {!isLoading ? (        <div className="h-[400px] w-full">
             {media_type=== "video" ? <iframe height="100%" width="100%" src={url} ></iframe> 
             : 
-            <img src={url} alt={`apod-${title}`} className="h-full object-cover w-full" />  }
+            <img src={url} alt={`apod-${title}`} className="h-full rounded-lg object-cover w-full" />  }
         </div>) : <div className="h-[400px] w-full grid place-content-center"> 
-        <Skeleton className="h-[400px] w-[1200px]  " />
+        <Skeleton className="h-[400px]  w-[400px]" />
         </div>  }
 
-        <button onClick={nextHandler}>
-            <CircleArrowRight/>
+        <button onClick={nextHandler}className="mx-4" disabled={day===0}>
+            <CircleArrowRight size={36} color={`${day===0 ? "gray" : "black"}`}  className={`${day===0 ? "gray" : "hover:scale-110 transition-all"}`}/>
         </button>
     </div>
     <div className="capitalize text-center text-2xl" >{date}</div>
-    <div className="mx-auto w-full my-8" >
-        <p className="capitalize text-2xl mb-2">{title}</p>
-        <p className="capitalize">{explanation}</p>
-        <p className="capitalize text-right">{date}</p>
+    <div className="mx-auto w-full my-8 flex flex-col gap-2 " >
+        <p className="capitalize text-2xl text-center mb-2">{title}</p>
+        <p className="capitalize text-center p-5">{explanation}</p>
+        <p className="capitalize text-right p-5">{date}</p>
 
     </div>
     
